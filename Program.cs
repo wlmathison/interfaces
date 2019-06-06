e4483b168fbc1b14e6ca06fbd216ec26b3f3850d
Billy Mathison <wlmathison@gmail.com>
2019-06-06 15:45:27 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
